fd2b129e387a6b932253c702375ee99bee0c7de9
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-09-14 13:10:19 -0700

    Update Reddit-Sentiment-Analysis.py

--- 05-Reddit-Sentiment-Analysis/Reddit-Sentiment-Analysis.py
+++ 05-Reddit-Sentiment-Analysis/Reddit-Sentiment-Analysis.py
@@ -30,4 +30,39 @@ for post in subreddit_stocks.hot(limit=5):   # Grabs info from "stock" title lim
             break
 
 
-# 3 most uploaded comments in those hot posts
+def get_titles_and_comments(subreddit='stocks', limit=6, num_comments=3, skip_first=2):   # skip_first are pinned comments
+    subreddit = reddit.subreddit(subreddit)
+    title_and_comments = {}
+
+    for counter,post in enumerate(subreddit.hot(limit=limit)):
+
+        if counter < skip_first:
+            continue
+        counter += (1-skip_first)
+
+        title_and_comments[counter] = ""
+        # PRAW Documentation
+        submission = reddit.submission(post.id)
+        title = post.title
+
+        title_and_comments[counter] += "Title: " + title + "\n\n"
+        title_and_comments[counter] += "Comments: \n\n"
+
+        comment_counter = 0
+        for comment in submission.comments:
+            if not comment.body == "[deleted]":
+                title_and_comments[counter] += comment.body + "\n"
+                comment_counter += 1
+            if comment_counter == num_comments:
+                break
+    return title_and_comments
+
+
+titles_and_comments = get_titles_and_comments()
+print(titles_and_comments)
+
+
+def create_prompt(title_and_comments):
+    task = "Return the stock ticker or company name mentioned in the following comments and classify the sentiment " \
+           "around the company as positive, negative, or neutral. If no ticker or company is mentioned write 'No company" \
+           "mentioned' \n\n "
