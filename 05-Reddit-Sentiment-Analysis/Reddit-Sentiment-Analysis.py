41103e595e990f9941a19054225715bea93ac117
Vicente Ceja <105320673+veechente95@users.noreply.github.com>
2023-09-13 22:47:20 -0700

    Update Reddit-Sentiment-Analysis.py

--- 05-Reddit-Sentiment-Analysis/Reddit-Sentiment-Analysis.py
+++ 05-Reddit-Sentiment-Analysis/Reddit-Sentiment-Analysis.py
@@ -11,6 +11,23 @@ reddit = praw.Reddit(
     user_agent="Sentiment analysis test"
 )
 
-# Connect to redit and grab information from "stock" title limited to 5
-for submission in reddit.subreddit("stock").hot(limit=5):
-    print(submission.title)
+subreddit_stocks = reddit.subreddit("stock")
+
+# access the first 5 hot posts
+for post in subreddit_stocks.hot(limit=5):   # Grabs info from "stock" title limited to 5
+    print(post.title)
+    submission = reddit.submission(post.id)
+    # print top 2 comments per tittle submission
+    counter = 0
+    for comment in submission.comments:
+        print(comment.body)   # text of comment
+        # skips over deleted comments
+        if comment.body == ["deleted"]:
+            pass
+        # Let's say we don't want to surpass more than 2 comments per title
+        counter += 1
+        if counter == 2:
+            break
+
+
+# 3 most uploaded comments in those hot posts
